5c3afa0622321456c19eaa3dc3a4f42c6b719438
sanikasail00 <sailsanika98@gmail.com>
2024-11-10 15:35:02 +0530

    Add files via upload

---
+++ src/components/FeaturedCake.js
@@ -0,0 +1,14 @@
+// src/components/FeaturedCake.js
+import React from 'react';
+
+function FeaturedCake() {
+  return (
+    <section id="featured" className="featured-cake">
+      <h2>Featured Cake of the Month</h2>
+      <p>Chocolate Delight - Rich, creamy, and irresistible!</p>
+      <button>Add to Cart</button>
+    </section>
+  );
+}
+
+export default FeaturedCake;
